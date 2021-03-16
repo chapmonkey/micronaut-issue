3de886ef4a6878172627f12725c3d809380fc1a2
Nick Chapman <nick.chapman@albanyeast.com>
2021-03-16 15:51:10 +0000

    Empty project

---
+++ src/test/java/queryvalueissue/QueryvalueissueTest.java
@@ -0,0 +1,21 @@
+package queryvalueissue;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class QueryvalueissueTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
